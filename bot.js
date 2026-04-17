5d4d5ef6b831eaa63d031ef78db59a99635f86ed
21s123-eng <hxlcvbn123@gmail.com>
2026-04-17 17:41:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1232,7 +1232,7 @@ client.on('channelUpdate', async (oldChannel, newChannel) => {
 });
 
 client.on('guildMemberUpdate', async (oldMember, newMember) => {
-        const nonEveryoneRoles = newMember.roles.cache.filter(r => r.id !== newMember.guild.id);
+    const nonEveryoneRoles = newMember.roles.cache.filter(r => r.id !== newMember.guild.id);
     if (nonEveryoneRoles.size === 0 && !newMember.user.bot) {
         await newMember.roles.add(UNVERIFIED_ROLE_ID).catch(() => {});
     }
